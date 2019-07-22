063319c2a35946c0b0090fff49c47b8faf79c420
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2019-07-22 21:23:48 +0800

    Update random-site.js

--- random-site.js
+++ random-site.js
@@ -14,6 +14,22 @@ if (thing == 'https://what-if.xkcd.com/')
   }
 }
 
+if (thing == 'https:youtube.com/')
+{
+  watif = Math.floor(Math.random()*2);
+  if (watif > 0)
+  {
+var alpha =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'];
+var randomalpha = 0; 
+var linkrandomyou = '';
+for(let i = 0; i < 11; i++){
+randomalpha = Math.floor(Math.random()*(alpha.length));
+linkrandomyou += alpha[randomalpha];
+}
+    thing = thing + 'watch?v=' + linkrandomyou;
+  }
+}
+
 
 var linki = document.getElementById('linkrandom');
 linki.setAttribute('href', thing);
