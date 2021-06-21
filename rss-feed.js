7444b7ccb1d9464f8ff439cddaf93471c872355d
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2021-06-21 17:27:06 +0800

    Add files via upload

---
+++ rss-feed.js
@@ -0,0 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(){var e=window,s="";for(i=0;i<e.rssfeed_url.length;i++)s=s+"rssfeed[url]["+i+"]="+encodeURIComponent(e.rssfeed_url[i])+"&";var r=("https:"==document.location.protocol?"https":"http")+"://feed.surfing-waves.com/php/rssfeed.php"+"?"+s+"rssfeed[type]="+(e.rssfeed_type?e.rssfeed_type:"")+"&rssfeed[frame_width]="+e.rssfeed_frame_width+"&rssfeed[frame_height]="+e.rssfeed_frame_height+"&rssfeed[scroll]="+(e.rssfeed_scroll?e.rssfeed_scroll:"")+"&rssfeed[scroll_step]="+(e.rssfeed_scroll_step?e.rssfeed_scroll_step:"")+"&rssfeed[scroll_bar]="+(e.rssfeed_scroll_bar?e.rssfeed_scroll_bar:"")+"&rssfeed[target]="+(e.rssfeed_target?e.rssfeed_target:"")+"&rssfeed[font_size]="+(e.rssfeed_font_size?e.rssfeed_font_size:"")+"&rssfeed[font_face]="+(e.rssfeed_font_face?e.rssfeed_font_face:"")+"&rssfeed[border]="+(e.rssfeed_border?e.rssfeed_border:"")+"&rssfeed[css_url]="+(e.rssfeed_css_url?encodeURIComponent(e.rssfeed_css_url):"")+"&rssfeed[title]="+(e.rssfeed_title?e.rssfeed_title:"")+"&rssfeed[title_name]="+(e.rssfeed_title_name?encodeURIComponent(e.rssfeed_title_name):"")+"&rssfeed[title_bgcolor]="+(e.rssfeed_title_bgcolor?encodeURIComponent(e.rssfeed_title_bgcolor):"")+"&rssfeed[title_color]="+(e.rssfeed_title_color?encodeURIComponent(e.rssfeed_title_color):"")+"&rssfeed[title_bgimage]="+(e.rssfeed_title_bgimage?encodeURIComponent(e.rssfeed_title_bgimage):"")+"&rssfeed[footer]="+(e.rssfeed_footer?e.rssfeed_footer:"")+"&rssfeed[footer_name]="+(e.rssfeed_footer_name?encodeURIComponent(e.rssfeed_footer_name):"")+"&rssfeed[footer_bgcolor]="+(e.rssfeed_footer_bgcolor?encodeURIComponent(e.rssfeed_footer_bgcolor):"")+"&rssfeed[footer_color]="+(e.rssfeed_footer_color?encodeURIComponent(e.rssfeed_footer_color):"")+"&rssfeed[footer_bgimage]="+(e.rssfeed_footer_bgimage?encodeURIComponent(e.rssfeed_footer_bgimage):"")+"&rssfeed[item_bgcolor]="+(e.rssfeed_item_bgcolor?encodeURIComponent(e.rssfeed_item_bgcolor):"")+"&rssfeed[item_bgimage]="+(e.rssfeed_item_bgimage?encodeURIComponent(e.rssfeed_item_bgimage):"")+"&rssfeed[item_title_length]="+(e.rssfeed_item_title_length?e.rssfeed_item_title_length:"")+"&rssfeed[item_title_color]="+(e.rssfeed_item_title_color?encodeURIComponent(e.rssfeed_item_title_color):"")+"&rssfeed[item_border_bottom]="+(e.rssfeed_item_border_bottom?e.rssfeed_item_border_bottom:"")+"&rssfeed[item_source_icon]="+(e.rssfeed_item_source_icon?e.rssfeed_item_source_icon:"")+"&rssfeed[item_date]="+(e.rssfeed_item_date?e.rssfeed_item_date:"")+"&rssfeed[item_description]="+(e.rssfeed_item_description?e.rssfeed_item_description:"")+"&rssfeed[item_description_length]="+(e.rssfeed_item_description_length?e.rssfeed_item_description_length:"")+"&rssfeed[item_description_color]="+(e.rssfeed_item_description_color?encodeURIComponent(e.rssfeed_item_description_color):"")+"&rssfeed[item_description_link_color]="+(e.rssfeed_item_description_link_color?encodeURIComponent(e.rssfeed_item_description_link_color):"")+"&rssfeed[item_description_tag]="+(e.rssfeed_item_description_tag?e.rssfeed_item_description_tag:"")+"&rssfeed[no_items]="+(e.rssfeed_no_items?e.rssfeed_no_items:"")+"&rssfeed[cache]="+(e.rssfeed_cache?e.rssfeed_cache:"");document.write('<iframe name="rssfeed_frame" width="'+e.rssfeed_frame_width+'" height="'+e.rssfeed_frame_height+'" frameborder="0" src="'+r+'" marginwidth="0" marginheight="0" vspace="0" hspace="0" scrolling="no" ALLOWTRANSPARENCY="true"></iframe>')}();
+
+
+}
+/*
+     FILE ARCHIVED ON 23:26:02 May 31, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 09:25:09 Jun 21, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 712.521
+  exclusion.robots: 0.111
+  exclusion.robots.policy: 0.103
+  RedisCDXSource: 1.671
+  esindex: 0.009
+  LoadShardBlock: 252.546 (3)
+  PetaboxLoader3.datanode: 293.057 (5)
+  CDXLines.iter: 111.138 (3)
+  load_resource: 229.61 (2)
+  PetaboxLoader3.resolve: 179.375 (2)
+*/
